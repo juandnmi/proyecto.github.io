add8746afd99afe3ecb382c4537135127ab93a6a
Dangod-spec <131696115+Dangod-spec@users.noreply.github.com>
2024-10-29 09:05:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ function mostrarImagen() {
             : seleccion === "imagen2"
             ? "https://media.gq.com.mx/photos/5f6ce732bc946e88f6c96320/16:9/w_1600,c_limit/goky%20ultra%20instinto.jpg"
             : "https://media.istockphoto.com/id/959978484/es/foto/rojo-redondo-en-blanco-bot%C3%B3n-rojo.jpg?s=612x612&w=0&k=20&c=PMMi8GEH6csl0oD8QMBL9pLG3-UQQglYDJThK9eOATE=";
-        
+
         imagenSeleccionada.appendChild(img);
     }
 }
 
 function irHome() {
-    window.location.href = '/'; // Cambia '/' por la URL de tu página de inicio si es diferente
+    // Restablece el menú desplegable a la opción inicial y limpia la imagen
+    document.getElementById("menuDesplegable").selectedIndex = 0; // Selecciona la opción "Selecciona una imagen"
+    mostrarImagen(); // Limpia la imagen mostrada
 }
