8f25020641cf1c32d782205ae7012cc5c5ca2e23
juandnmi <juandiegopg88@gmail.com>
2024-11-01 13:17:58 -0500

    Update datos_localidades.js

--- datos_localidades.js
+++ datos_localidades.js
@@ -12,5 +12,26 @@ var localidades = [
         "longitud": -74.10000,
         "casos": 200
     },
+    // Datos de casos en Sumapaz
+var sumapazCasos = [
+    { año: 2012, casos: 7 },
+    { año: 2013, casos: 24 },
+    { año: 2014, casos: 29 },
+    { año: 2015, casos: 17 },
+    { año: 2016, casos: 22 },
+    { año: 2017, casos: 13 },
+    { año: 2018, casos: 25 },
+    { año: 2019, casos: 29 },
+    { año: 2020, casos: 15 },
+    { año: 2021, casos: 14 },
+    { año: 2022, casos: 8 },
+    { año: 2023, casos: 18 },
+    { año: 2024, casos: 17 },
+    { año: 2025, casos: 17 },
+    { año: 2026, casos: 16 },
+    { año: 2027, casos: 16 },
+    { año: 2028, casos: 15 }
+];
+
     // Agrega más localidades según tus datos
 ];
