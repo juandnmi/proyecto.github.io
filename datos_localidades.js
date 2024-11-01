f8db08504f9e1e36ea37d9e9cfa53616be296077
juandnmi <juandiegopg88@gmail.com>
2024-11-01 13:12:49 -0500

    Create datos_localidades.js

---
+++ datos_localidades.js
@@ -0,0 +1,16 @@
+// Array de objetos de localidades con datos de cada una
+var localidades = [
+    {
+        "nombre": "Localidad 1",
+        "latitud": 4.60971,  // Coordenadas de ejemplo
+        "longitud": -74.08175,
+        "casos": 150
+    },
+    {
+        "nombre": "Localidad 2",
+        "latitud": 4.63000,
+        "longitud": -74.10000,
+        "casos": 200
+    },
+    // Agrega más localidades según tus datos
+];
